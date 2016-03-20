9ca5c7683747bf68fba02764f5d5de0921b659fb
Marteen21 <znmarteen21@gmail.com>
2016-03-20 18:25:40 +0100

    Added Homework4

---
+++ Homework4_pi.m
@@ -0,0 +1,20 @@
+% W = [1;1;0]
+% V = [0 1 0;2 0 0]
+% VIO = [0 1 0;2 0 0;1 2 0]
+% VIW = [V W]
+% P = VIO*inv(VIW)
+
+%%%% DansGame
+clear
+v1=randi(3,3,1)
+v2=randi(3,3,1)
+v3=randi(3,3,1)
+
+V = [v1,v2]
+W = [v3]
+U = [V,W]
+if rank(U)~=3
+    error('restart m8, the rank is not quite right sry m8')
+else
+    P= [V,zeros(3,1)]*inv(U)
+end
