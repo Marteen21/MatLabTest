83700fa020e3cbc9f98bf4905f150779450dbfad
Marteen21 <znmarteen21@gmail.com>
2016-04-26 00:02:34 +0200

    Test2

--- rektimus_ep9.m
+++ rektimus_ep9.m
@@ -1,6 +1,6 @@
 %% Matematika 9. házi feladat
 %% (a) feladat
-% Generáljunk véletlen elemekkel egy ilyen P mátrixot és egy véletlen 
+% Generáljunk véletlen elemekkel egy ilyen P mátrixot és egy véletlen
 % nemnegatív vv vektort (legyen ?v?1=1?v?1=1)! Milyen pozitivitási
 % osztályba tartozik a P mátrix (primitív, reducibilis, irreducibilis)?
 clear all;
@@ -12,25 +12,21 @@ for i = 1:n
         P(i,i+1) = 1-pe;
         P(i,n) = pe;
     elseif (i == 12)
-            P(i,1) = 1-pe;
-            P(i,n-1) = pe;
+        P(i,1) = 1-pe;
+        P(i,n-1) = pe;
     else
         P(i,i-1) = pe;
         P(i,i+1) = 1-pe;
     end
 end
-             
 
-sum = 0;    
-v = rand(1,n)
-for i = 1:n
-   sum = sum + v(i); 
-end
-v = v/sum;
+
 sum = 0;
+v = rand(1,n)
 for i = 1:n
     sum = sum + v(i);
 end
+v = v/sum;
 P
 v
 
@@ -38,13 +34,32 @@ v
 % P mátrix irreducibilis, mert a belõle felírt mátrix minden élbõl bejárható
 
 %% (b) feladat
-% Vizsgáljuk meg a vPnvPn vektorok viselkedését, ha nn tart végtelenhez! 
-% Létezik-e e vektorsorozatnak határértéke vagy torlódási pontja? 
-% Létezik-e a v,vP,…,vPk?1v,vP,…,vPk?1 vektorok átlagának határértéke? 
-% Mi ezek jelentése? 
-for i=1:700
+% Vizsgáljuk meg a vPnvPn vektorok viselkedését, ha nn tart végtelenhez!
+% Létezik-e e vektorsorozatnak határértéke vagy torlódási pontja?
+% Létezik-e a v,vP,…,vPk?1v,vP,…,vPk?1 vektorok átlagának határértéke?
+% Mi ezek jelentése?
+
+for i=1:1000    %A végtelen esetet egy igen nagy (1000 db) os esettel közelítettem
     limesN = v*P^i
+    if(mod(i,2)==0)
+        figure(1)
+        title('Páros hatványok');
+        plot(limesN)
+        hold on
+        
+    else
+        figure(2)
+        title('Páratlan hatványok');
+        plot(limesN)
+        hold on
+    end
 end
+
+%%
+% A vektorsorozat szemmel láthatóan torlódik, határértéke ennek ellenére
+% nincsen
+
+%% (c) feladat
 r = max(eig(P))
 [p_root q] = perron(P,'left')
 
@@ -59,7 +74,7 @@ A = -V;
 b = zeros(size(V,1),1);
 sum = 0;
 for i=1:n
-   sum = sum + (V(i)); 
+    sum = sum + (V(i));
 end
 Aeq = sum;
 beq = 1;
